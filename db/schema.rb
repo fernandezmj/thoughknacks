93faf9aca67ded6795c316f5aace4500e5bd4b25
Dahrel Salud <dahrelsalud@gmail.com>
2016-03-31 15:07:36 +0800

    generate devise views for user, update registration view add first name, last nam, and birthday. Add column in user birthday

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160331042944) do
+ActiveRecord::Schema.define(version: 20160331064917) do
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20160331042944) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.date     "birthday"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
