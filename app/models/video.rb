ef9d9a9afa5bd5bf79cf651ab9bd07a1caa60162
michael <fernandez.michaeljohn@gmail.com>
2016-04-08 08:49:25 +0800

    fixes spaces; corrects index

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,10 +1,10 @@
 class Video < ActiveRecord::Base
-	belongs_to :category
+  belongs_to :category
 
-	validates :title, :video_link, :thumbnail, :category_id, :video_link, presence: true
-	validates :title, :uniqueness => {:case_sensitive => false}
+  validates :title, :video_link, :thumbnail, :category_id, :video_link, presence: true
+  validates :title, :uniqueness => {:case_sensitive => false}
 
-	mount_uploader :thumbnail, ThumbnailUploader
+  mount_uploader :thumbnail, ThumbnailUploader
   
   mount_uploader :video_link, VideoLinkUploader 
 end
